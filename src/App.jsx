6d9ea5f1f9b43b7317e053019db34476cc007253
faisalKhan <faisaldev404@gmail.com>
2024-05-15 16:17:32 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <div>
+
+      <h1 className="text-3xl font-bold underline text-center pt-2 hover:bg-green-800 hover:text-slate-100">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
