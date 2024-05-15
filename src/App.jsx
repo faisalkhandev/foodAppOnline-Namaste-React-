9b87c636e80cac8c7a41c174298e3fefe4d8e8b5
faisalKhan <faisaldev404@gmail.com>
2024-05-15 17:00:14 +0500

    add: added menu, tailwind and basic setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { Header } from "./components"
 
 
 const App = () => {
   return (
     <div>
 
-      <h1 className="text-3xl font-bold underline text-center pt-2 hover:bg-green-800 hover:text-slate-100">
-        Hello world!
-      </h1>
+      <Header />
+
 
     </div>
   )
